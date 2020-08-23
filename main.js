6b91a01e0125e9b50e69166f6d8b97f1886fa662
Ruslan Bulyna <rus.bulyna@gmail.com>
2020-08-23 10:31:40 +0300

    Homework 3

--- main.js
+++ main.js
@@ -1,29 +1,103 @@
-const firstRow = prompt('Введите первую строку');
-const secondRow = prompt('Введите вторую строку');
+const btnNewGame = document.getElementById('btn-new-game');
+const btnKickCharacter = document.getElementById('btn-kick-pokemon1');
+const btnKickEnemy = document.getElementById('btn-kick-pokemon2');
+const btnHealCharacter = document.getElementById('btn-heal-pokemon1');
+const btnHealEnemy = document.getElementById('btn-heal-pokemon2');
 
-function getRow(firstRow, secondRow) {
-    let num1 = 0, num2 = 0;
-    for (let index = 0; index < firstRow.length; index++) {
-        if(firstRow.charAt(index) != 'a' || firstRow.charAt(index) != 'A'){
-            num1++;
-        }
-        
+const character = {
+    name: 'Pikachu',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-character'),
+    elProgressbar: document.getElementById('progressbar-character'),
+    elKickBtn: btnKickCharacter,
+    elHealBtn: btnHealCharacter,
+    healDefaultCount: 3,
+    healDamageHPCount: 3
+}
+
+const enemy = {
+    name: 'Charmander',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-enemy'),
+    elProgressbar: document.getElementById('progressbar-enemy'),
+    elKickBtn: btnKickEnemy,
+    elHealBtn: btnHealEnemy,
+    healDefaultCount: 3,
+    healDamageHPCount: 3
+}
+
+btnNewGame.addEventListener('click', function(){
+    initGame();
+});
+
+btnKickCharacter.addEventListener('click', function(){
+    changeHP(random(10), enemy);
+});
+
+btnKickEnemy.addEventListener('click', function(){
+    changeHP(random(10), character);
+});
+
+btnHealCharacter.addEventListener('click', function(){
+    healHP(character, btnHealCharacter);
+});
+
+btnHealEnemy.addEventListener('click', function(){
+    healHP(enemy, btnHealEnemy);
+});
+
+function renderHP(person){
+    person.elHP.innerText = person.damageHP + ' / ' + person.defaultHP;
+    person.elProgressbar.style.width = person.damageHP + '%';
+}
+
+function renderHealCount(person, btn){
+    btn.innerText = `Полечить (${person.healDamageHPCount})`;
+}
+
+function changeHP(count, person){
+    if(person.damageHP < count){
+        person.damageHP = 0;
+        alert(`К сожалению ${person.name} проиграл!`)
+        person.elKickBtn.disabled = true;
+        person.elHealBtn.disabled = true;
+    } else {
+        person.damageHP -= count;   
     }
-    for (let index = 0; index < secondRow.length; index++) {
-        if(secondRow.charAt(index) != 'a' || secondRow.charAt(index) != 'A'){
-            num2++;
-        }
-    }
-    if(num1 > num2 ) {
-        return `В первой строке больше символов 'a'`;
-    }
-    else if(num1 < num2){
-        return `Во второй строке больше символов 'a'`;
-    }
-    else {
-        return `Количество символов 'a' в первой строке равно количеству символов 'a' во второй`;
-    }
+    renderHP(person);
     
 }
 
-console.log(getRow(firstRow, secondRow)); 
+function healHP(person, btn){
+    if(person.healDamageHPCount !== 0 && (person.defaultHP - person.damageHP) >= 10) {
+        person.damageHP += 10;
+        person.healDamageHPCount--;
+    } else {
+        person.elHealBtn.disabled = true;
+    }
+    renderHealCount(person, btn);
+    renderHP(person);
+}
+
+function random(num){
+    return Math.ceil(Math.random() * num);
+}
+
+function newGame(person){
+    person.damageHP = person.defaultHP;
+    person.healDamageHPCount = person.healDefaultCount;
+}
+
+function initGame(){
+    console.log('Start Game');
+    newGame(character);
+    newGame(enemy);
+    renderHP(character);
+    renderHP(enemy);
+    renderHealCount(character, btnHealCharacter);
+    renderHealCount(character, btnHealEnemy);
+}
+
+initGame();
